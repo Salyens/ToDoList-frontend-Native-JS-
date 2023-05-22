cceec496131c444e92b32f406bae1cb851366b08
Salyens <Salyens@yandex.ru>
2023-05-22 21:41:45 +0400

    check and sort completed

--- js/script.js
+++ js/script.js
@@ -14,39 +14,45 @@ class ToDo {
 
     addToDo(toDoItem) {
         this.list.push(toDoItem);
-        this.list.sort((a, b) => a.checked > b.checked ? 1 : -1)
-        console.log(this.list);
-
         localStorage.setItem('toDolist', JSON.stringify(this.list));
     }
 
-
      addLi(value, id, checked) {
-        const ul = document.querySelector('ul');
-        const li = document.createElement('li');
-        const editDeleteDiv = document.createElement('div');
-        const editSpan = document.createElement('span');
-        const deleteSpan = document.createElement('span');
-        const input = document.createElement('input');
-        const doneDiv = document.createElement('div');
-        editDeleteDiv.className = 'edit-delete';
-        editSpan.className = 'edit-icon';
-        deleteSpan.className = 'delete-icon';
-        li.setAttribute('data-id', id);
-        editSpan.innerHTML = '<i class="fa fa-edit">';
-        deleteSpan.innerHTML = '<i class="fa fa-trash-o"></i>';
-        editDeleteDiv.append(editSpan);
-        editDeleteDiv.append(deleteSpan);
-        li.append(editDeleteDiv);
-        ul.append(li);
-        input.className = 'to-do-text';
-        input.value = value;
-        li.append(input);
-        doneDiv.className = 'done';
-        doneDiv.innerHTML = '<i class="fa fa-check-circle"></i>';
-        li.append(doneDiv);
+        let deleteSpan;
+        let editSpan;
+        let doneDiv;
 
-        if(checked) input.classList.add('line-through');
+        const createLiHTML = () => {
+            const ul = document.querySelector('ul');
+            const li = document.createElement('li');
+            const editDeleteDiv = document.createElement('div');
+            editSpan = document.createElement('span');
+            deleteSpan = document.createElement('span');
+            const input = document.createElement('input');
+            doneDiv = document.createElement('div');
+            editDeleteDiv.className = 'edit-delete';
+            editSpan.className = 'edit-icon';
+            deleteSpan.className = 'delete-icon';
+            li.setAttribute('data-id', id);
+            editSpan.innerHTML = '<i class="fa fa-edit">';
+            deleteSpan.innerHTML = '<i class="fa fa-trash-o"></i>';
+            editDeleteDiv.append(editSpan);
+            editDeleteDiv.append(deleteSpan);
+            li.append(editDeleteDiv);
+            ul.append(li);
+            input.className = 'to-do-text';
+            input.value = value;
+            li.append(input);
+            doneDiv.className = 'done';
+            doneDiv.innerHTML = '<i class="fa fa-check-circle"></i>';
+            li.append(doneDiv);
+
+            if(checked) {
+                input.classList.add('line-through');
+                input.disabled = true;
+            }
+        }
+        createLiHTML();
 
         deleteSpan.addEventListener('click', (event) => {
             const index = event.target.parentElement.parentElement.parentElement.getAttribute('data-id');
@@ -57,21 +63,20 @@ class ToDo {
             localStorage.setItem('toDolist', JSON.stringify(this.list));
         });
 
-
         doneDiv.addEventListener('click', (event) => {
             const index = event.target.parentElement.parentElement.getAttribute('data-id');
-            event.target.parentElement.parentElement.querySelector('input').classList.add('line-through');
             const findToDoItem = this.list.find(item => item.id == index);
-            findToDoItem.checked = true;
-            this.addAndDisplayListItem();
+            findToDoItem.checked = !findToDoItem.checked;
             localStorage.setItem('toDolist', JSON.stringify(this.list));
+            this.addAndDisplayListItem();
         })
     }   
 
     addAndDisplayListItem() {
         let ul = document.querySelector('ul');
         ul.innerHTML = '';
-        this.list.sort((a, b) => a.checked > b.checked ? 1 : -1);
+        this.list.sort((a, b) => a.id - b.id);
+        this.list.sort((a, b) => a.checked - b.checked);
         for (let i = 0; i < this.list.length; i++) {
             let value = this.list[i].text;
             let id = this.list[i].id
@@ -88,12 +93,12 @@ class ToDo {
             const checkValue = (value) => {
                 for (const el of this.list) {
                     if(el.text === value) {
-                        this.msg.innerText = 'This deal has already added';
+                        this.msg.innerText = 'This task is already added';
                         return;
                     }
                 }
                 if(!value.length) {
-                    this.msg.innerText = 'Enter your deal';
+                    this.msg.innerText = 'Enter your task';
                 }
                 else {
                     this.msg.innerText = '';
